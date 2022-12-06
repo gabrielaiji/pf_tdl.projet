9af9261428947de1d0ade91124f5463117812cb4
jlavaur <lavaurjonas@gmail.com>
2022-12-06 16:09:36 +0100

    Passe génération du code sans fonctions + faire tests + faire signatures fonctions

---
+++ sourceEtu/passeCodeRatToTam.ml
@@ -0,0 +1,128 @@
+(* Module de la passe de génération du code *)
+(* doit être conforme à l'interface Passe *)
+open Tds
+open Ast.AstType
+open Ast.AstPlacement
+open Type
+open Code
+open Tam
+
+
+type t1 = Ast.AstPlacement.programme
+type t2 = string
+
+let rec analyse_code_expression e =
+  match e with
+  | AppelFonction(info_ast,le) ->
+    let c = String.concat "" (List.map analyse_code_expression le) in
+      (match info_ast_to_info info_ast with
+      | InfoFun(name,_,_) -> c ^ (call "ST" name)
+      | _ -> failwith "InternalError")
+  
+  | Ident info_ast ->
+    (match info_ast_to_info info_ast with
+    | InfoVar(_,t,dep,reg) -> load (getTaille t) dep reg
+    | InfoConst(_,i) -> loadl_int i
+    | _ -> failwith "InternalError")
+
+  | Booleen b ->
+    if b then loadl_int 1
+         else loadl_int 0
+
+  | Entier i -> loadl_int i
+
+  | Unaire(op,e) ->
+    let c = analyse_code_expression e in
+      c ^ (match op with
+      | Numerateur -> pop 0 1
+      | Denominateur -> pop 1 1)
+
+  | Binaire(op,e1,e2) ->
+    let c1 = analyse_code_expression e1 in
+      let c2 = analyse_code_expression e2 in
+        c1 ^ c2 ^ (match op with
+        | Fraction -> call "ST" "norm"
+        | PlusInt -> subr "IAdd"
+        | PlusRat -> call "ST" "RAdd"
+        | MultInt -> subr "IMul"
+        | MultRat -> call "ST" "RMul"
+        | EquInt -> subr "IEq"
+        | EquBool -> subr "IEq"
+        | Inf -> subr "ILss")
+
+
+let rec analyse_code_instruction i =
+  match i with
+  | Declaration(info_ast,e) ->
+    (match info_ast_to_info info_ast with
+    | InfoVar(_,t,d,r) ->
+      push (getTaille t) ^ (analyse_code_expression e) ^ (store (getTaille t) d r)
+    | _ -> failwith "InternalError")
+  
+  | Affectation(info_ast,e) ->
+    (match info_ast_to_info info_ast with
+    | InfoVar(_,t,d,r) ->
+      (analyse_code_expression e) ^ (store (getTaille t) d r)
+    | _ -> failwith "InternalError")
+  
+  | AffichageInt e ->
+    let ne = analyse_code_expression e in
+      ne ^ subr "IOut"
+
+  | AffichageRat r ->
+    let nr = analyse_code_expression r in
+      nr ^ call "ST" "ROut"
+
+  | AffichageBool b ->
+    let nb = analyse_code_expression b in
+      nb ^ subr "BOut"
+
+  | Conditionnelle(c,b1,b2) ->
+    let nc = analyse_code_expression c in
+      let els = getEtiquette() in
+        let endif = getEtiquette() in
+          let nb1 = analyse_code_bloc b1 in
+            let nb2 = analyse_code_bloc b2 in
+              nc
+              ^ (jumpif 0 els)
+              ^ nb1
+              ^ (jump endif)
+              ^ (label els)
+              ^ nb2
+              ^ (label endif)
+
+  | TantQue(c,b) ->
+    let nc = analyse_code_expression c in
+      let whil = getEtiquette() in
+        let endwhil = getEtiquette() in
+          let nb = analyse_code_bloc b in
+            (label whil)
+            ^ nc
+            ^ (jumpif 0 endwhil)
+            ^ nb
+            ^ (jump whil)
+            ^ (label endwhil)
+
+  | Retour(e,tailleRet,tailleParams) ->
+    let ne = analyse_code_expression e in
+      ne ^ return tailleRet tailleParams
+
+  | Empty -> ""
+
+  and analyse_code_bloc (li,tailleBloc) =
+    let c = String.concat "" (List.map analyse_code_instruction li) in
+      c ^ (pop 0 tailleBloc)
+
+
+let analyse_code_fonction f = failwith "TO DO"
+
+
+let analyser (Programme (fonctions,prog)) =
+  let enTete = getEntete() in
+    let fct = String.concat "" (List.map analyse_code_fonction fonctions) in
+      let progs = analyse_code_bloc prog in
+        enTete
+        ^ fct
+        ^ label "main"
+        ^ progs
+        ^ halt
